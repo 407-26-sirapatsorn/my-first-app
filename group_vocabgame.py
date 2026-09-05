a5d9b6fbf74174c6b75673a961175b9ebdb9494c
407-26-sirapatsorn <54613@yupparaj.ac.th>
2026-09-05 22:42:16 +0700

    Update group_vocabgame.py

--- group_vocabgame.py
+++ group_vocabgame.py
@@ -1,7 +1,7 @@
 import time
 import streamlit as st
 
-st.title("⏱️ เกมเติมศัพท์จับเวลา")
+st.title("⏱️ เกมทายคำศัพท์ภาษาอังกฤษหมวด ประเทศ")
 
 if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
